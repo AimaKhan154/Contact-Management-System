1971ce1cd9064f91eab4d6f16b399e5c997037c7
AimaKhan154 <aimakhan6456@gmail.com>
2026-05-19 03:49:36 -0700

    Add files via upload

---
+++ backend/src/main/java/org/contactmgmt/ContactMgmtApplication.java
@@ -0,0 +1,13 @@
+package org.contactmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMgmtApplication.class, args);
+	}
+
+}
